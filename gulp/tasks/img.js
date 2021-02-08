5217f32f514fb8f478c879b5b8de53d5e907b090
Dmitriy <dmitriy1597531@gmail.com>
2021-02-08 19:23:38 +0200

    Last project state

---
+++ gulp/tasks/img.js
@@ -0,0 +1,7 @@
+module.exports = function () {
+  $.gulp.task('img', function () {
+    return $.gulp
+      .src('src/static/img/**/*.{png,jpg,gif,svg}')
+      .pipe($.gulp.dest('build/img'))
+  })
+}
